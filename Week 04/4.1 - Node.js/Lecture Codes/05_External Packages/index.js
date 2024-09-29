5852e42a46159010f54995c4b3e41d760e6b5b6d
arjunkumar811 <kumararjun26401@gmail.com>
2024-09-29 15:24:56 +0530

    05_External Packages Pushed

---
+++ Week 04/4.1 - Node.js/Lecture Codes/05_External Packages/index.js
@@ -0,0 +1,10 @@
+import chalk from 'chalk';
+
+console.log(chalk);
+console.log(chalk.blue('Hello world!'));
+console.log(chalk.red.bold('This is an error message.'));
+console.log(chalk.green.underline('This is a success message'));
+
+import express from 'express';
+
+console.log(express);
