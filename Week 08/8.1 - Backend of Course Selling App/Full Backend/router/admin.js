6d34a23c78008294d9880f2b9d8830411bc30524
arjunkumar811 <kumararjun26401@gmail.com>
2024-10-01 23:43:07 +0530

    Added Zod

--- Week 08/8.1 - Backend of Course Selling App/Full Backend/router/admin.js
+++ Week 08/8.1 - Backend of Course Selling App/Full Backend/router/admin.js
@@ -9,6 +9,12 @@ const { JWT_ADMIN_PASSWORD } = require("../config");
 
 
 adminRouter.post("/signup", adminMiddleware, function(req, res){
+    const requireBody = zod.object({
+        email: zod.string().email().min(5), // Email must be a valid format and at least 5 characters
+        password: zod.string().min(5), // Password must be at least 5 characters
+        firstName: zod.string().min(3), // First name must be at least 3 characters
+        lastName: zod.string().min(3), // Last name must be at least 3 characters
+    });
 
     res.json({
     message: "SignUp Done"
