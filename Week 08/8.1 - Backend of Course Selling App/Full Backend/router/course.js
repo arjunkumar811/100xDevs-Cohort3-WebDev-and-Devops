2df8d2740419850dc2a6f58ca97e17281e739319
arjunkumar811 <kumararjun26401@gmail.com>
2024-10-02 12:03:43 +0530

    Purchase Endpoint Pushed

--- Week 08/8.1 - Backend of Course Selling App/Full Backend/router/course.js
+++ Week 08/8.1 - Backend of Course Selling App/Full Backend/router/course.js
@@ -6,13 +6,36 @@ const { purchaseModel, courseModel } = require("../db");
 const { userMiddleware } = require("../middlewares/user");
 
 
+courseRouter.post("/purchase", userMiddleware, async function(req, res){
+  const userId = userId;
+  const courseId =req.body.courseId;
 
-courseRouter.post("/purchase", function(req, res){
-res.json({
-  message: "Purchase endpoint"
+  if (!courseId) {
+    return res.status(400).json({
+        message: "Please provide a courseId", // Error message sent back to the client
+    });
+}
+
+const existingPurchase = await purchaseModel.findOne({
+  courseId: courseId,
+  userId: userId,
+});
+
+if (existingPurchase) {
+  return res.status(400).json({
+      message: "You have already bought this course",
+  });
+}
+
+await purchaseModel.create({
+  courseId: courseId, 
+  userId: userId,   
+});  
+
+res.status(201).json({
+  message: "You have successfully bought the course",
 });
 });
-
 
 courseRouter.post("/preview", function(req, res){
     res.json({
