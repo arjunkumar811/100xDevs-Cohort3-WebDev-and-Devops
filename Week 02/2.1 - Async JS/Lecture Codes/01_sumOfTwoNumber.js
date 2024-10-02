9cffc52b8df065df0054524b54713bb9c6369d91
arjunkumar811 <kumararjun26401@gmail.com>
2024-10-02 22:12:40 +0530

    Added Lecturesmcode folder

---
+++ Week 02/2.1 - Async JS/Lecture Codes/01_sumOfTwoNumber.js
@@ -0,0 +1,11 @@
+// function to add two number and return the sum
+function sum(a, b){
+    return a+b;
+}
+
+
+// calling the function and storing the result in a vaiable
+let ans = sum(3, 5);
+
+// printing the result
+console.log(ans); //5
