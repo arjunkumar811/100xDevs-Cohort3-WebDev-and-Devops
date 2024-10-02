18023104f4827a9bcb196da13e7bdb5994023362
arjunkumar811 <kumararjun26401@gmail.com>
2024-10-02 23:01:17 +0530

    Op function

---
+++ Week 02/2.1 - Async JS/Lecture Codes/11_functionalArgumentApproach2.js
@@ -0,0 +1,26 @@
+// function to add two numbers
+function sum(a, b) {
+    return a + b;
+}
+
+// function to sub two numbers
+function sum(a, b) {
+    return a - b;
+}
+
+// function to mul two numbers
+function sum(a, b) {
+    return a * b;
+}
+
+// function to division two numbers
+function sum(a, b) {
+    return a / b;
+}
+
+
+function doOperation(a, b, op) {
+    let val = op(a, b);
+    return val;
+}
+
