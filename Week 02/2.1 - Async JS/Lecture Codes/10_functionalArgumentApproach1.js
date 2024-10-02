35e990dd1ce06a6a3f2cd96cebecbde6fa048bb4
arjunkumar811 <kumararjun26401@gmail.com>
2024-10-02 22:53:47 +0530

    Function

---
+++ Week 02/2.1 - Async JS/Lecture Codes/10_functionalArgumentApproach1.js
@@ -0,0 +1,31 @@
+// function to add two numbers
+function sum(a, b){
+    return a + b;
+}
+
+// function to substract two numbers
+function sum(a, b){
+    return a - b;
+}
+
+// function to multiply two numbers
+function sum(a, b){
+    return a * b;
+}
+
+// function divide two numbers
+function sum(a, b){
+    return a / b;
+}
+
+// function to do operation on two numbers
+function doOperation(a, b, op) {
+    // return op(a, b);
+    let val = op(a, b);
+}
+
+// print the sum of 1 and 2
+console.log(sum(1, 2)); // 3
+
+// print the multiplication of 2 and 5
+console.log(multiply(2, 5)); // 10
