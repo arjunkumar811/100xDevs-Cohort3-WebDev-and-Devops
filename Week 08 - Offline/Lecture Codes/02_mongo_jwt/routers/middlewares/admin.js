14c70cc528f18670229ceaf20cdc9cbfdab3e7c8
arjunkumar811 <kumararjun26401@gmail.com>
2024-10-05 19:12:31 +0530

    Added try catch

--- Week 08 - Offline/Lecture Codes/02_mongo_jwt/routers/middlewares/admin.js
+++ Week 08 - Offline/Lecture Codes/02_mongo_jwt/routers/middlewares/admin.js
@@ -9,5 +9,24 @@ const token = req.headers.authorization;
 const words = token.split(" "); 
     const jwtToken = words[1]; 
 
+    try {
+      
+        const decodedValue = jwt.verify(jwtToken, JWT_SECRET);
+
+      
+        if (decodedValue.username) {
+            next(); 
+        } else {
+            res.status(403).json({
+                message: "You are not authenticated", 
+            });
+        }
+    } catch (e) {
+        res.status(401).json({ 
+            message: "Incorrect inputs", 
+        });
+    }
+}
+
 module.exports = adminMiddleware;  
 
