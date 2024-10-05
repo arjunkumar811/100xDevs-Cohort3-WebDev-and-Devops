c3a374a327c01b928ae57199063ae872e0b49581
arjunkumar811 <kumararjun26401@gmail.com>
2024-10-05 13:52:45 +0530

    Added existingUser End point

--- Week 08 - Offline/Lecture Codes/01_mongo/routers/user.js
+++ Week 08 - Offline/Lecture Codes/01_mongo/routers/user.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const { UserModel } = require("../db");
 
 const userrouter = express.Router();
 
-userrouter.post("/signup", function(req, res){
+userrouter.post("/signup", async function(req, res){
 const username = req.body.username;
 const password = req.body.password;
+
+const existingUser = await UserModel.findOne({
+username,
+});
+
+if(existingUser) {
+    return res.status(400).json({
+    message: "User already exist",
+    });
+}
 });
 
 
