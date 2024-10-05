08df4812d3354316b5bbfc6b1121a42906b820c7
arjunkumar811 <kumararjun26401@gmail.com>
2024-10-05 18:41:09 +0530

    existingAdmin

--- Week 08 - Offline/Lecture Codes/02_mongo_jwt/routers/admin.js
+++ Week 08 - Offline/Lecture Codes/02_mongo_jwt/routers/admin.js
@@ -1,11 +1,14 @@
 const express = require("express");
+const { Admin } = require("../db");
 const userRouter = express.Router();
 
-userRouter.post("/signup", function(req, res){
+userRouter.post("/signup", async function(req, res){
 const username = req.body.username;
 const password = req.body.password;
 
-
+const existingAdmin = await Admin.findOne({
+username: username,
+});
 
 });
 
