57c11e45d43e96464a118f977663c22ef94fdf26
arjunkumar811 <kumararjun26401@gmail.com>
2024-10-05 15:13:13 +0530

    Created UserSchema

--- Week 08 - Offline/Lecture Codes/02_mongo_jwt/db.js
+++ Week 08 - Offline/Lecture Codes/02_mongo_jwt/db.js
@@ -4,3 +4,14 @@ const AdminSchema = new mongoose.Schema({
 username: { type: String, unique: true },
 password: { type: String },
 });
+
+const UserSchema = new monggose.Schema({
+    username: { type: String, unique: true },
+    password: { type: String },
+    purchasedCourses: [
+        {
+            type: mongoose.Schema.Types.ObjectId, 
+            ref: "Course",
+        },
+    ],
+});
