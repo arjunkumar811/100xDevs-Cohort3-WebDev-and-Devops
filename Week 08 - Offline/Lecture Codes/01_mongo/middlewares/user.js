352e937705da99422b37a448f860ef661b6d1eb6
arjunkumar811 <kumararjun26401@gmail.com>
2024-10-05 12:15:09 +0530

    findOne

--- Week 08 - Offline/Lecture Codes/01_mongo/middlewares/user.js
+++ Week 08 - Offline/Lecture Codes/01_mongo/middlewares/user.js
@@ -4,5 +4,10 @@ const { UserModel } = require("../db");
 function userMiddleware(req, res, next){
  const username = req.headers.username;
  const password = req.headers.password;
+  
+ UserModel.findOne({
+    username: username,
+    password: password,
+ });
 
 };
