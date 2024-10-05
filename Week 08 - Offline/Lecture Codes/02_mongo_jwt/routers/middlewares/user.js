69afd108ae58998270cd24fde89f1755a12ec39a
arjunkumar811 <kumararjun26401@gmail.com>
2024-10-05 19:18:27 +0530

    Added token

--- Week 08 - Offline/Lecture Codes/02_mongo_jwt/routers/middlewares/user.js
+++ Week 08 - Offline/Lecture Codes/02_mongo_jwt/routers/middlewares/user.js
@@ -4,4 +4,7 @@ const { JWT_SECRET } = require("../../config");
 
 function userMiddleware(req, res, next) {
  const token = req.headers.authorization;
+
+ const words = token.split(" "); 
+ const jwtToken = words[1];
 };
