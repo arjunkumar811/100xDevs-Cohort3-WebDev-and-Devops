cec50ba75a4f1b7a41fb8397c9e3691d2ffa56d4
arjunkumar811 <kumararjun26401@gmail.com>
2024-10-08 01:55:00 +0530

    Push the counter code in react

--- Week 09/9.1 - Intro to React/Lecture Codes/03_Counter app-react/Counter App/src/App.jsx
+++ Week 09/9.1 - Intro to React/Lecture Codes/03_Counter app-react/Counter App/src/App.jsx
@@ -4,13 +4,23 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+
+function onClickHeadler() {
+setCount(count + 1);
+}
+
 
   return (
     <>
-     
+    <h1>Counter App</h1>
+     <button onClick={onClickHeadler}> 
+
+     Counter {count}
+     </button>
     </>
-  )
+  );
 }
 
 export default App
