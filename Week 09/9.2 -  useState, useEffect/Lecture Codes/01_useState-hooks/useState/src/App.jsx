51b6435bd5949570a5a73c6c68fd46ba2db117d0
arjunkumar811 <kumararjun26401@gmail.com>
2024-10-08 21:21:30 +0530

    Code logic Added

--- Week 09/9.2 -  useState, useEffect/Lecture Codes/01_useState-hooks/useState/src/App.jsx
+++ Week 09/9.2 -  useState, useEffect/Lecture Codes/01_useState-hooks/useState/src/App.jsx
@@ -10,22 +10,22 @@ function App() {
 }
 
 function Counter() {
+// Create a state variable called count and a function to update it called setCount 
+const [count, setCount] = useState(0);  
 
 
 
 
 function incresecount() {
-
+  setCount(count + 1);
 }
 
 function decreasecount() {
-
-
+  setCount(count - 1);
 }
 
 function reset() {
-
-
+  setCount(0);
 }
 
 return (
