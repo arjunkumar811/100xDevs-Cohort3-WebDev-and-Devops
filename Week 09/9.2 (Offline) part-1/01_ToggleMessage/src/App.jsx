3b824e23411d4cc6673e2fa92412b96af667343a
arjunkumar811 <kumararjun26401@gmail.com>
2024-10-10 19:59:33 +0530

    Renamed... Folder

---
+++ Week 09/9.2 (Offline) part-1/01_ToggleMessage/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <ToggleMessage/>
+    <ToggleMessage/>
+    <ToggleMessage/>
+    </>
+  )
+}
+
+function ToggleMessage(){
+const [isVisiable, setVisiable] = useState(true);
+
+function toggle(){
+  setVisiable(!isVisiable);
+}
+
+return(
+  <>
+  <button onClick={toggle}>
+    Toggle Messsage
+  </button>
+  {isVisiable && <p>This Message is conditionally rendered</p>}
+  </>
+)
+}
+
+export default App
