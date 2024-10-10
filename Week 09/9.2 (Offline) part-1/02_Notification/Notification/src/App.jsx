e44c24b0f9698af3d3cefcf6fbad4de5fa799e82
arjunkumar811 <kumararjun26401@gmail.com>
2024-10-10 22:36:36 +0530

    Added NOtification logic code

--- Week 09/9.2 (Offline) part-1/02_Notification/Notification/src/App.jsx
+++ Week 09/9.2 (Offline) part-1/02_Notification/Notification/src/App.jsx
@@ -4,30 +4,20 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(1);
+ 
+
+  function icresecount() {
+   setCount(count + 1);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div style={{background: "red", borderRadius: 20, width:20 , height: 25, paddingLeft: 10, paddingTop: 5 }}>
+      {count}
+    </div>
+     <img src="https://www.seekpng.com/png/detail/130-1304578_notification-comments-notification-icon-svg.png" alt="" width={50}/>
+     <button onClick={icresecount}> Increse </button>
     </>
   )
 }
