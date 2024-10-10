d021e2c45fa08c71ac9bff3267107f309c8fe655
arjunkumar811 <kumararjun26401@gmail.com>
2024-10-10 17:57:43 +0530

    Vite Project created

--- Week 09/9.2 -  useState, useEffect/Lecture Codes/03_conditional-rendering/condition/src/App.jsx
+++ Week 09/9.2 -  useState, useEffect/Lecture Codes/03_conditional-rendering/condition/src/App.jsx
@@ -3,44 +3,57 @@ import { useState, useEffect } from "react";
 
 
 function App() {
-   
-    return (
-        <div>
-        <Counter />
-        </div>
-    );
-}
 
-
-function Counter() {
-    
-    const [count, setCount] = useState(0);
-
-    console.log("Counter Rendered");    
+    let [counterVisible, setCounterVisible] = useState(true);
 
   
     useEffect(() => {
      
-        let clock = setInterval(() => {
+        setInterval(() => {
            
+            setCounterVisible((c) => !c);
+        }, 5000);
+    }, []); 
+    return (
+        <div>
+            <h1>Conditional Rendering</h1>
+
+          
+            {counterVisible && <Counter />}
+        </div>
+    );
+}
+
+function Counter() {
+   
+    const [count, setCount] = useState(0);
+
+    console.log("Counter Rendered");    
+
+    
+    useEffect(() => {
+       
+        let clock = setInterval(() => {
+         
             setCount((count) => count + 1);
         }, 1000);
 
-  
+        
         console.log("Mounted");
 
-      
+   
         return function () {
             console.log("Unmounted");
 
-            
+         
             clearInterval(clock); 
         };
-    }, []); 
-    
+    }, []);
 
+ 
     return (
         <div>
+           
             <h1>{count}</h1>
         </div>
     );
