5c987bdcd66dcc2f28f61aacf5745457be0791c9
arjunkumar811 <kumararjun26401@gmail.com>
2024-10-11 01:37:17 +0530

    Created Component to project

---
+++ Week 09/9.2 (Offline) part-1/04_Linkedin like topbar/04_LinkedIn/src/App.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { useEffect } from 'react';
+
+function App() {
+  
+
+  
+
+  return (
+    <>
+      <button onClick={function() {
+        setCurrentTab(1)
+      }} style={{color: currentTab == 1 ? "red" : "black" }}>Todo #1</button>
+      <button onClick={function() {
+        setCurrentTab(2)
+      }} style={{color: currentTab == 2 ? "red" : "black" }}>Todo #2</button>
+      <button onClick={function() {
+        setCurrentTab(1)
+      }} style={{color: currentTab == 3 ? "red" : "black" }}>Todo #3</button>
+      <button onClick={function() {
+        setCurrentTab(1)
+      }} style={{color: currentTab == 4 ? "red" : "black" }}>Todo #4</button>
+    </>
+  )
+}
+
+export default App
