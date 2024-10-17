28120b1e1963359e18ba0474c0ee726256390925
arjunkumar811 <kumararjun26401@gmail.com>
2024-10-18 02:45:25 +0530

    Added Component

--- Week 09/9.2 (Offline) part -2/Modals/02_Modals/src/App.jsx
+++ Week 09/9.2 (Offline) part -2/Modals/02_Modals/src/App.jsx
@@ -3,31 +3,51 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+
+const Modal = ({ isOpen, onClose, children }) => {
+  if(!isOpen) return null;
+
+
+  return (
+    <div style={{
+      position: 'fixed',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      backgroundColor: 'rgba(0, 0, 0, 0.5)',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+  }}>
+      <div style={{
+          background: 'white',
+          padding: '20px',
+          borderRadius: '5px',
+      }}>
+          <button onClick={onClose}>Close</button>
+          {children}
+      </div>
+  </div>
+  );
+};
+
+
+
+
+
+
+
+
+
+
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
