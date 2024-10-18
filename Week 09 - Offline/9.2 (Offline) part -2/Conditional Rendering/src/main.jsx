0b92225666df4f66b2fb75367e3075a6a77fb194
arjunkumar811 <kumararjun26401@gmail.com>
2024-10-18 23:06:31 +0530

    Conditiona Rendering!

--- Week 09 - Offline/9.2 (Offline) part -2/Conditional Rendering/src/main.jsx
+++ Week 09 - Offline/9.2 (Offline) part -2/Conditional Rendering/src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import ToggleMessage from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ToggleMessage />
   </StrictMode>,
 )
