959d8f1853cc263f27b903d36116aee8e7f72695
arjunkumar811 <kumararjun26401@gmail.com>
2024-10-18 22:34:36 +0530

    Props

--- Week 09/9.2 (Offline) part -2/props/src/App.jsx
+++ Week 09/9.2 (Offline) part -2/props/src/App.jsx
@@ -1,16 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-
-
-  return (
-    <>
-      
-    </>
-  )
+const App = () => {
+  return <Greeting name="Arjun"/>
 }
 
+const Greeting = ({ name }) => {
+   return <h1> Hello, {name}!, </h1>
+}
+ 
+
+ 
 export default App
