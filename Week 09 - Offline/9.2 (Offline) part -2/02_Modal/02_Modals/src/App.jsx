4cc1a8e9b854af3654f77934339ffb2459f3f8f0
arjunkumar811 <kumararjun26401@gmail.com>
2024-10-18 23:24:37 +0530

    Conditional Rendering

--- Week 09 - Offline/9.2 (Offline) part -2/02_Modal/02_Modals/src/App.jsx
+++ Week 09 - Offline/9.2 (Offline) part -2/02_Modal/02_Modals/src/App.jsx
@@ -32,30 +32,22 @@ const Modal = ({ isOpen, onClose, children }) => {
   </div>
   );
 };
-
-
-const App = () => {
   
+  
+const App = () => {
+  const [isModalOpen, setModalOpen] = useState(false);
 
   return (
-    
+    <div>
+       <button  onClick={() => setModalOpen(true)}>Open Modal</button>
+      <Modal isOpen={isModalOpen} onClose={() => setModalOpen(false)}>
+        <h2>Modal Title</h2>
+        <p>This is some content inside the modal</p>
+      </Modal>
+    </div>
   )
 }
 
 
 
-
-
-
-
-
-function App() {
-
-  return (
-    <>
-      
-    </>
-  )
-}
-
-export default App
+export default App;
