55c6fe788b2a5fc69523fcd82cf1d8c2bae7a4ba
arjunkumar811 <kumararjun26401@gmail.com>
2024-10-20 01:47:25 +0530

    Layout

--- Week 10/10.1/Week 10 - Routing/src/App.jsx
+++ Week 10/10.1/Week 10 - Routing/src/App.jsx
@@ -2,23 +2,26 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link , Outlet} from "react-router-dom";
 
 function App() {
   
 
   return (
-    <>
+    <div>
       <BrowserRouter>
       <Routes>
-        <Route path='/'element={<Layout />} />
+        
+        <Route path='/'element={<Layout />} >
 <Route path='/neet/online-coaching-class-11' element = {<Class11Program/>} />
 <Route path='/neet/online-coaching-class-12' element = {<Class12Program/>} />
 <Route path='/' element = {<Landing/>} />
 <Route path='*' element={<ErrorPage/>} />
+</Route>
       </Routes>
+      Footer | Contact us
       </BrowserRouter>
-    </>
+    </div>
   )
 
 
@@ -28,18 +31,23 @@ function App() {
 <div style={{height: "90vh", background: "red"}}>
   <Outlet />
 </div>
-footer
+<footer>
+  footer
+  </footer>
+
     </div>
   }
 
 
 
   function Header() {
+   return <div>
 <Link to="/">Allen</Link>
 | 
 <Link to="/neet/online-coaching-class-11">Class 11</Link>
 |
 <Link to="/neet/online-coaching-class-12">Class 12</Link>
+</div>
 }
 
   function ErrorPage() {
