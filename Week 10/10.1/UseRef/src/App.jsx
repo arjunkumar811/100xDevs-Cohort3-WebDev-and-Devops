f7bbd64818abccab7e86ce95873e407e610626cf
arjunkumar811 <kumararjun26401@gmail.com>
2024-10-20 18:45:29 +0530

    State and ref

--- Week 10/10.1/UseRef/src/App.jsx
+++ Week 10/10.1/UseRef/src/App.jsx
@@ -3,25 +3,42 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function FocusInput() {
-  // Step 1: Create a ref to store the input element
-  const inputRef = useRef(null);
+// function FocusInput() {
+//   // Step 1: Create a ref to store the input element
+//   const inputRef = useRef(null);
 
-  // Step 2: Define the function to focus the input
-  const handleFocus = () => {
-  // Access the DOM node and call the focus method
-  inputRef.current.focus();
-  };
+//   // Step 2: Define the function to focus the input
+//   const handleFocus = () => {
+//   // Access the DOM node and call the focus method
+//   inputRef.current.focus();
+//   };
 
 
 
-  return (
-    <div>
-      <input ref={inputRef} type="text" placeholder='Click Me' />
-      <button onClick={handleFocus}>Focus the input</button>
-    </div>
-  )
+//   return (
+//     <div>
+//       <input ref={inputRef} type="text" placeholder='Click Me' />
+//       <button onClick={handleFocus}>Focus the input</button>
+//     </div>
+//   )
+// }
+
+
+function Chat() {
+  const [messages, setMessages] = useState(["Hello!, "How are you?]);
+  const chatBoxRef = useRef(null);
+
+
+  
 }
+
+
+
+
+
+
+
+
 
 function Apps() {
   return (
