972d0c35320cc5db5882f4764cc99fcc225aa18b
arjunkumar811 <kumararjun26401@gmail.com>
2024-11-01 23:17:42 +0530

    Added vite code

--- Week 10/Context API - 10.2/State-expriments/src/App.jsx
+++ Week 10/Context API - 10.2/State-expriments/src/App.jsx
@@ -54,3 +54,60 @@ const { Bulbon, setBulbon } = useContext(BulbContext);
 }
 
 export default App;
+
+
+
+// CONSTEXT API NOT STRUCTURED
+
+// function App() { 
+//   const [Bulbon, setBulbon] = useState(true);
+
+
+//  return (  
+//  <div>
+//   <BulbContext.Provider value={{
+//     Bulbon: Bulbon,
+//     setBulbon: setBulbon
+//   }}> 
+//   <LightBulb />
+//   </BulbContext.Provider>
+//  </div>
+//  );
+// }
+
+
+// function LightBulb() {
+//   return (
+//     <div>
+//       <BulbState />
+//       <ToggleBulbState />
+//     </div>
+//   );
+// }
+
+
+// function BulbState(){
+
+//   const { Bulbon } = useContext(BulbContext);
+//   return <div>
+// {Bulbon ? "Bulb On" : "Bulb Off"}
+//   </div>
+// }
+
+
+// function ToggleBulbState() {
+
+// const { Bulbon, setBulbon } = useContext(BulbContext);
+
+//   function Toggle() {
+//     setBulbon(!Bulbon);
+//     } 
+
+//   return <div>
+//     <button onClick={Toggle}>Toggle the bulb</button>
+//   </div>
+// }
+
+// export default App;
+
+
