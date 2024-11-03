2b50d2d8b211d4fdc55028cc0a59980c5d82b967
arjunkumar811 <kumararjun26401@gmail.com>
2024-11-04 00:14:30 +0530

    Fetch

--- Week 11/11.1 - Custom Hooks/Lecture Codes/02_useFetch-custom-hooks/src/App.jsx
+++ Week 11/11.1 - Custom Hooks/Lecture Codes/02_useFetch-custom-hooks/src/App.jsx
@@ -1,14 +1,26 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  
+  const [posts, setPosts] = useState({});
+
+ async function getPosts() {
+  const response = await fetch ("https://jsonplaceholder.typicode.com/todos/1");
+  const json = await response.json();
+  setPosts(json);
+}
+
+
+  useEffect(() => {
+   getPosts();
+  }, []) 
 
   return (
     <>
-     hi there
+    {posts.userId}
+    {posts.title}
     </>
   )
 }
