70f036f6ebf3df9e32014ded3569045c3ad6269d
arjunkumar811 <kumararjun26401@gmail.com>
2024-11-13 17:48:21 +0530

    Added memo Code

--- Week 11/11.2 - Recoil/Lecture Codes/03_memo-in-react/src/App.jsx
+++ Week 11/11.2 - Recoil/Lecture Codes/03_memo-in-react/src/App.jsx
@@ -1,13 +1,57 @@
-import { useState } from 'react'
+import { useEffect, useState, memo } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      
+      <Counter />
     </>
-  )
+  );
 }
 
-export default App
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  // Start interval to increment count every 3 seconds
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount((c) => c + 1);
+    }, 3000);
+
+    // Cleanup the interval on unmount
+    return () => clearInterval(interval);
+  }, []);
+
+  // Increment and decrement functions
+  const increment = () => setCount((prevCount) => prevCount + 1);
+  const decrement = () => setCount((prevCount) => prevCount - 1);
+
+  return (
+    <div>
+      <CurrentCount count={count} />
+      <Increament increment={increment} />
+      <Decrease decrement={decrement} />
+    </div>
+  );
+}
+
+const CurrentCount = memo(function ({ count }) {
+  return <div>{count}</div>;
+});
+
+const Increament = memo(function ({ increment }) {
+  return (
+    <div>
+      <button onClick={increment}>Increase</button>
+    </div>
+  );
+});
+
+const Decrease = memo(function ({ decrement }) {
+  return (
+    <div>
+      <button onClick={decrement}>Decrease</button>
+    </div>
+  );
+});
+
+export default App;
