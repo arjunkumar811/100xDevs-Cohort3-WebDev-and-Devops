7861a0d927018f7860b253aa88d70f2b969d8076
arjunkumar811 <kumararjun26401@gmail.com>
2024-11-15 00:29:15 +0530

    Added Raw way of selectors

--- Week 11/11.3 offline Recoil Deep Dive/Leacture Code/Selectors/src/App.jsx
+++ Week 11/11.3 offline Recoil Deep Dive/Leacture Code/Selectors/src/App.jsx
@@ -1,16 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRecoilValue, RecoilRoot, useRecoilState, useSetRecoilState, atoms } from 'recoil';
+import { jobsAtom, MesssageAtom, NetworkAtom, NotificationAtom } from './atom1';
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  return (
+    <RecoilRoot>
+      <MainApp />
+    </RecoilRoot>
+  );
+}
+
+function MainApp() {
+  const networkNotificationCount = useRecoilValue(NetworkAtom);
+  const jobsCount = useRecoilValue(jobsAtom);
+  const messageCount = useRecoilValue(MesssageAtom);
+  const notificationCount = useRecoilValue(NotificationAtom);
+
+
+  // useMemo
+  const  finalTotal = networkNotificationCount + jobsCount + messageCount + notificationCount;
+
 
   return (
     <>
-      
+      <button>Home</button>
+      <button>My Network {networkNotificationCount >= 100 ? "99+" : networkNotificationCount}</button>
+      <button>Jobs {jobsCount}</button>
+      <button>Messaging {messageCount}</button>
+      <button>Notification {notificationCount}</button>
+      <button onClick={finalTotal}>Me</button>
     </>
-  )
+  );
 }
 
-export default App
+
+
+export default App;
+
+
+////////////////////////////////////////RAW WAY//////////////////////////////////////////////
+
+
+
+// function MainApp() {
+//   const networkNotificationCount = useRecoilValue(NetworkAtom);
+//   const jobsCount = useRecoilValue(jobsAtom);
+//   const messageCount = useRecoilValue(MesssageAtom);
+//   const notificationCount = useRecoilValue(NotificationAtom);
+//   const  finalTotal = networkNotificationCount + jobsCount + messageCount + notificationCount;
+
+
+//   return (
+//     <>
+//       <button>Home</button>
+//       <button>My Network {networkNotificationCount >= 100 ? "99+" : networkNotificationCount}</button>
+//       <button>Jobs {jobsCount}</button>
+//       <button>Messaging {messageCount}</button>
+//       <button>Notification {notificationCount}</button>
+//       <button onClick={finalTotal}>Me</button>
+//     </>
+//   );
+// }
