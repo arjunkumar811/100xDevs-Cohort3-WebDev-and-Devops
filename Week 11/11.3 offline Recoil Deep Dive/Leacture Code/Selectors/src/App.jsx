e77d00fefa0694503373a9f067d8aa0d4387ad04
arjunkumar811 <kumararjun26401@gmail.com>
2024-11-15 00:35:49 +0530

    useMemo for selector logic

--- Week 11/11.3 offline Recoil Deep Dive/Leacture Code/Selectors/src/App.jsx
+++ Week 11/11.3 offline Recoil Deep Dive/Leacture Code/Selectors/src/App.jsx
@@ -1,42 +1,62 @@
 import { useRecoilValue, RecoilRoot, useRecoilState, useSetRecoilState, atoms } from 'recoil';
 import { jobsAtom, MesssageAtom, NetworkAtom, NotificationAtom } from './atom1';
+import { useMemo } from 'react';
 
 
 
-function App() {
-  return (
-    <RecoilRoot>
-      <MainApp />
-    </RecoilRoot>
-  );
-}
-
-function MainApp() {
-  const networkNotificationCount = useRecoilValue(NetworkAtom);
-  const jobsCount = useRecoilValue(jobsAtom);
-  const messageCount = useRecoilValue(MesssageAtom);
-  const notificationCount = useRecoilValue(NotificationAtom);
 
 
-  // useMemo
-  const  finalTotal = networkNotificationCount + jobsCount + messageCount + notificationCount;
+//////////////////////////////////////USE MEMO////////////////////////////////////////////////////////////
 
 
-  return (
-    <>
-      <button>Home</button>
-      <button>My Network {networkNotificationCount >= 100 ? "99+" : networkNotificationCount}</button>
-      <button>Jobs {jobsCount}</button>
-      <button>Messaging {messageCount}</button>
-      <button>Notification {notificationCount}</button>
-      <button onClick={finalTotal}>Me</button>
-    </>
-  );
-}
+// function App() {
+//   return (
+//     <RecoilRoot>
+//       <MainApp />
+//     </RecoilRoot>
+//   );
+// }
+
+// function MainApp() {
+//   const networkNotificationCount = useRecoilValue(NetworkAtom);
+//   const jobsCount = useRecoilValue(jobsAtom);
+//   const messageCount = useRecoilValue(MesssageAtom);
+//   const notificationCount = useRecoilValue(NotificationAtom);
+
+
+//   // useMemo
+//   const  finalTotal =  useMemo(() => {
+//     return useMenetworkNotificationCount + jobsCount + messageCount + notificationCount;
+//   }, [useMenetworkNotificationCount, jobsCount, messageCount, notificationCount]);
+
+
+//   return (
+//     <>
+//       <button>Home</button>
+//       <button>My Network {networkNotificationCount >= 100 ? "99+" : networkNotificationCount}</button>
+//       <button>Jobs {jobsCount}</button>
+//       <button>Messaging {messageCount}</button>
+//       <button>Notification {notificationCount}</button>
+//       <button onClick={finalTotal}>Me</button>
+//     </>
+//   );
+// }
 
 
 
-export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 ////////////////////////////////////////RAW WAY//////////////////////////////////////////////
