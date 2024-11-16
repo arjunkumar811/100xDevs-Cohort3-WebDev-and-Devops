6bdc575101defd4ac6dff514b5969d9c6f8b407d
arjunkumar811 <kumararjun26401@gmail.com>
2024-11-17 00:11:38 +0530

    Grid Added

--- Week 13/13.1 - Tailwind Part 1/Lecture Codes/01_tailwind/src/Grids.jsx
+++ Week 13/13.1 - Tailwind Part 1/Lecture Codes/01_tailwind/src/Grids.jsx
@@ -1,12 +1,12 @@
 function Grid() {
     return (
       <div>
-          <h2 className="mt-4 text-[30px] font-bold">Grid Example using Tailwind</h2>
+          <h2 className=" font-bold">Grid Example using Tailwind</h2>
   
-          <div className="grid grid-cols-12">
-              <div className="bg-blue-600 col-span-4 p-2">Child 1</div>
-              <div className="bg-red-600 col-span-6 p-2">Child 2</div>
-              <div className="bg-yellow-600 col-span-2 p-2">Child 3</div>
+          <div className="grid grid-cols-12 sm:col-span-5 ">
+              <div className="bg-blue-600 col-span-12 sm:col-span-5 ">Child 1</div>
+              <div className="bg-red-600 col-span-12 sm:col-span-5 ">Child 2</div>
+              <div className="bg-yellow-600 col-span-12 sm:col-span-5 ">Child 3</div>
           </div>
       </div>
     )
