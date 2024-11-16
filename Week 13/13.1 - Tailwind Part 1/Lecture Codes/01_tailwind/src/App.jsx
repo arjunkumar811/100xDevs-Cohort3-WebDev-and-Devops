be56183898c4ac1c0d3ff6cec18475813ba93f3a
arjunkumar811 <kumararjun26401@gmail.com>
2024-11-16 19:35:23 +0530

    Flex in tailwind

---
+++ Week 13/13.1 - Tailwind Part 1/Lecture Codes/01_tailwind/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className='flex justify-between'>
+        <div id="child1">
+        child1
+        </div>
+        <div id="child2">
+        child2
+        </div>
+        <div id="child3">
+        child3
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
