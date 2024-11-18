bf8027e45c3a51da33bebaf0b6d43a2dd28656b5
arjunkumar811 <kumararjun26401@gmail.com>
2024-11-19 01:34:15 +0530

    Week 12 Added

--- Week 13/13.1 - Tailwind Part 1/Lecture Codes/02-projects-using-taliwind/src/App.jsx
+++ Week 13/13.1 - Tailwind Part 1/Lecture Codes/02-projects-using-taliwind/src/App.jsx
@@ -1,13 +1,40 @@
 import './App.css'
-
 function App() {
- 
-
   return (
     <>
-      
+      <WebPage/>
     </>
   )
 }
+
+function WebPage() {
+  return (
+    <>
+    <div className="body bg-blue-900 flex justify-center items-center ">
+      
+      <div><h1>Webinar<span>.gg</span></h1>
+      </div>
+
+<div><h1>Verify Your Age</h1>
+</div>
+
+<div>
+<h3>please confirm your birth year. This data will not be stored</h3>
+</div>
+
+<div>
+<input type="button" value="Your Birth day year" />
+</div>
+
+<div>
+<button className=''>Continue</button>
+</div>
+
+    </div>
+    </>
+  )
+}
+
+
 
 export default App
