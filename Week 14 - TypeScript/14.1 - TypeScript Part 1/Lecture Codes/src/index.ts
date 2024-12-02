1b10e4000505401c9eff77e980be81ce4481bca1
arjunkumar811 <kumararjun26401@gmail.com>
2024-12-02 20:03:26 +0530

    TypeScript

---
+++ Week 14 - TypeScript/14.1 - TypeScript Part 1/Lecture Codes/src/index.ts
@@ -0,0 +1,49 @@
+// let x: number = 1;
+// console.log(x);
+
+//////////////////////////////////////////////////////
+// Write a function that greets a user given their first name. 
+
+// function greet(name : string | number) {
+// console.log("Hi Iam " + name);
+    
+// }
+
+// greet("Arjun");
+// greet(1);
+
+
+//////////////////////////////////////////////////
+// Problem 2 - Sum function
+
+// function sum(a : number , b : number) {
+//     return a + b;
+// }
+
+// let ans = sum(2, 3);
+// console.log(ans); // ans: 5
+
+//////////////////////////////////////////////////////////////
+// Problem 3 - Return true or false based on if a user is 18+
+
+// function isTrue(age: number) {
+//     if(age > 18) {
+//         return true;
+//     } else {
+//         return false
+//     }
+// }
+
+// console.log(isTrue(34));
+
+////////////////////////////////////////////////////////////////////////
+// Create a function that takes another function as input, and runs it after 1 second.
+
+
+// function delayedCall(fn: () => void) {
+//     setTimeout(fn, 1000);
+// }
+
+// delayedCall(function() {
+//     console.log("hi there");  // hi there
+// })
