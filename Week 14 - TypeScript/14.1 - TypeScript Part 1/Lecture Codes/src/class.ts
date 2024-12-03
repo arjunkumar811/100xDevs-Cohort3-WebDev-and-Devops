67391face8c38459f311c79e5762a97592a36285
arjunkumar811 <kumararjun26401@gmail.com>
2024-12-03 21:23:31 +0530

    Intersection

--- Week 14 - TypeScript/14.1 - TypeScript Part 1/Lecture Codes/src/class.ts
+++ Week 14 - TypeScript/14.1 - TypeScript Part 1/Lecture Codes/src/class.ts
@@ -14,18 +14,67 @@
 
 // Interafce
 
-interface UserType {
-    name: string; 
-    age: number;  
-}
+// interface UserType {
+//     name: string; 
+//     age: number;  
+// }
 
-function greet(user: UserType) { 
-    console.log("hi there " + user.name); 
-}
+// function greet(user: UserType) { 
+//     console.log("hi there " + user.name); 
+// }
 
-let user: UserType = {
-    name: "Arjun",
-    age: 20 
-};
+// let user: UserType = {
+//     name: "Arjun",
+//     age: 20 
+// };
 
-greet(user); 
+// greet(user); 
+
+
+// Types
+
+// type SumInput = string | number;
+
+// function sum(a :SumInput, b:SumInput) {
+//     return a + b;
+// }
+
+
+// Assignment #1 - Create a function isLegal that returns true or false if a user is above 18. It takes a user as an input.
+
+// interface User {
+// 	firstName: string;
+// 	lastName: string;
+// 	email: string;
+// 	age: number;
+// }
+
+// function isLegal(user: User) {
+//     if (user.age > 18) {
+//         return true
+//     } else {
+//         return false;
+//     }
+// }
+
+
+// Intersection
+
+// type Employee = {
+//     name: string;
+//     startDate: Date;
+//   };
+  
+//   type Manager = {
+//     name: string;
+//     department: string;
+//   };
+  
+//   type TeamLead = Employee & Manager;
+  
+//   const teamLead: TeamLead = {
+//     name: "harkirat",
+//     startDate: new Date(),
+//     department: "Software developer"
+//   };
+  
