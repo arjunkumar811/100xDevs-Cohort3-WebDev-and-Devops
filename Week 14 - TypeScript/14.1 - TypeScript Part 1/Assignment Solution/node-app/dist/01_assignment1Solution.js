9f933a13cd6f9eb97f76f4c6ee221ea0906b37b1
arjunkumar811 <kumararjun26401@gmail.com>
2024-12-04 02:32:05 +0530

    Added Assignment Solution

--- Week 14 - TypeScript/14.1 - TypeScript Part 1/Assignment Solution/node-app/dist/01_assignment1Solution.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// Assignment #1 - Write a function that calculates the sum of two arguments
-function sum(a, b) {
-    return a + b;
-}
-console.log(sum(2, 3)); //5
--- Week 14 - TypeScript/14.1 - TypeScript Part 1/Assignment Solution/node-app/dist/03_assignment3Solution.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// Create a function that takes another function as input, and runs it after 1 second.
-function delayedCall(fn) {
-    setTimeout(fn, 1000);
-}
-delayedCall(function () {
-    console.log("Arjun"); // arjun every one sec
-});
