7a5bb951cbec5319f00f62307d801eaa65e5eae4
arjunkumar811 <kumararjun26401@gmail.com>
2024-12-09 19:36:49 +0530

    ts

---
+++ Week 15/Lecture Code/brainly/src/index.ts
@@ -0,0 +1,38 @@
+
+import express from 'express';
+import  Jwt  from 'jsonwebtoken';
+
+const app = express();
+
+app.post("/api/v1/signup", (req, res) => {
+    
+})
+
+
+
+app.post("/api/v1/signin", (req, res) => {
+
+})
+
+
+app.post("/api/v1/content", (req, res) => {
+    
+})
+
+app.get("/api/v1/content", (req, res) => {
+    
+})
+
+app.delete("/api/v1/content", (req, res) => {
+    
+})
+
+
+
+app.post("/api/v1/brain/:share", (req, res) => {
+    
+})
+
+app.get("/api/v1/brain/:shareLink", (req, res) => {
+    
+})
