3a7fb87c8031b3db310c2db2161ed48fe043696a
arjunkumar811 <kumararjun26401@gmail.com>
2024-12-12 01:17:22 +0530

    Added Content delete endpoint

--- Week 15/Lecture Code/brainly/src/index.ts
+++ Week 15/Lecture Code/brainly/src/index.ts
@@ -146,8 +146,18 @@ app.get("/api/v1/content", userMiddleware, async (req, res) => {
     })
 })
 
-app.delete("/api/v1/content", (req, res) => {
-    
+app.delete("/api/v1/content", async userMiddleware, (req, res) => {
+    const contentId = req.body.contentId;
+
+    await ContentModel.deleteMany({
+        contentId,
+        //@ts-ignore
+        userId = req.userId
+    }) 
+
+    res.json({
+        message: "Message as been Deleted";
+    })
 })
 
 
