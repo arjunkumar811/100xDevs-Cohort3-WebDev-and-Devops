fb529bc7d15d0cf6a0554ae79da901f5a6460f64
arjunkumar811 <kumararjun26401@gmail.com>
2024-12-23 18:50:52 +0530

    Back-end code

--- Week 16 - Websockets/16.2 - WebSockets Project - Chat app/src/index.ts
+++ Week 16 - Websockets/16.2 - WebSockets Project - Chat app/src/index.ts
@@ -1,18 +1,24 @@
-import { WebSocketServer } from "ws";
+import { WebSocket, WebSocketServer } from "ws";
 
 
 const wss = new WebSocketServer({ port: 8000 });
 
 
  let userCount = 0;
+ let allSockets: WebSocket[] = [];
 
 wss.on("connection", (socket) => {
+allSockets.push(socket);
+
     userCount = userCount + 1;
     console.log("user connected #" + userCount);
 
     socket.on("message", (message) => {
         console.log("message recived " + message.toString());
-        socket.send(message.toString()+ ": sent from the server");
+        for (let i = 0; i<allSockets.length; i++) {
+            const s = allSockets[i];
+            s.send(message.toString() + ": sent from the server ")
+        }
     })
 
 })
