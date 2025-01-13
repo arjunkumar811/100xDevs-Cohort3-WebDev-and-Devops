09b37c6a8f602b49a41115291a8d0c9a4ef8bbdd
arjunkumar811 <kumararjun26401@gmail.com>
2025-01-14 00:43:08 +0530

    Added ws-setup

---
+++ Week 22 - Project #1 - Excalidraw/Week 22.1 - End to End - Project #1 - Excalidraw/Lecture Code/draw-application/apps/ws-backend/src/index.js
@@ -0,0 +1,9 @@
+import { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on('connection', function  connection(ws) {
+    ws.on('message', function message(data) {
+        ws.send("hi there")
+    })
+})
