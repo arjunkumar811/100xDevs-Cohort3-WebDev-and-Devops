4b434080b3c0d0ee1260ba31b9c584c8f009cc79
arjunkumar811 <kumararjun26401@gmail.com>
2025-01-15 22:17:31 +0530

    Added code

--- Week 22 - Project #1 - Excalidraw/Week 22.1 - End to End - Project #1 - Excalidraw/Lecture Code/draw-application/apps/ws-backend/src/index.ts
+++ Week 22 - Project #1 - Excalidraw/Week 22.1 - End to End - Project #1 - Excalidraw/Lecture Code/draw-application/apps/ws-backend/src/index.ts
@@ -2,28 +2,44 @@ import { WebSocketServer } from 'ws';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import { JWT_SECRET } from './config';
 
-
 const wss = new WebSocketServer({ port: 8080 });
 
 wss.on('connection', (ws, request) => {
     const url = request.url;
 
-    if(!url) {
-        return
+    if (!url) {
+        return;
     }
 
     const queryParams = new URLSearchParams(url.split('?')[1]);
     const token = queryParams.get('token');
 
-    const decoded = jwt.verify(token, JWT_SECRET)
-
-if(typeof decoded === "string") {
-    return
-}
-
-    if(!decoded && !(decoded as JwtPayload).username) {
+    if (!token) {
+        ws.send(JSON.stringify({
+            message: "Unauthorized"
+        }));
         ws.close();
-        return
+        return;
+    }
+
+    let decoded;
+    try {
+        decoded = jwt.verify(token, JWT_SECRET);
+    } catch (err) {
+        ws.send(JSON.stringify({
+            message: "Invalid token"
+        }));
+        ws.close();
+        return;
+    }
+
+    if (typeof decoded === "string") {
+        return;
+    }
+
+    if (!decoded || !(decoded as JwtPayload).username) {
+        ws.close();
+        return;
     }
 
     ws.on('message', (message) => {
