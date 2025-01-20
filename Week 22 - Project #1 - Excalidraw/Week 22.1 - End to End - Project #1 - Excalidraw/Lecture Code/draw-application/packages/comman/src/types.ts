c6a00a5e3d16a09d27271340c3a74473bc582d5b
arjunkumar811 <kumararjun26401@gmail.com>
2025-01-20 22:09:03 +0530

    ts

--- Week 22 - Project #1 - Excalidraw/Week 22.1 - End to End - Project #1 - Excalidraw/Lecture Code/draw-application/packages/comman/src/types.ts
+++ Week 22 - Project #1 - Excalidraw/Week 22.1 - End to End - Project #1 - Excalidraw/Lecture Code/draw-application/packages/comman/src/types.ts
@@ -1,13 +1,16 @@
+
 import { z } from "zod";
 
+
 export const CreateUserSchema = z.object({
-    username: z.string().min(3).max(30),
-    password: z.string()
+    email: z.string().min(3).max(30),
+    password: z.string(),
+    name: z.string()
 })
 
 
 export const SigninSchema = z.object({
-    username: z.string().min(3).max(30),
+    email: z.string().min(3).max(30),
     password: z.string()
 })
 
